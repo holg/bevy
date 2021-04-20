holg/bevy

commit 328fe3ba963284f706cd4e7d9466756ae760b3c6

Provide better error message when missing a render backend (#1965)
Fixes #626

crates/bevy_render/src/lib.rs
@@ -14,7 +14,7 @@ pub mod wireframe;
 
 use bevy_ecs::{
     schedule::{ParallelSystemDescriptorCoercion, SystemStage},
-    system::{IntoExclusiveSystem, IntoSystem},
+    system::{IntoExclusiveSystem, IntoSystem, Res},
 };
 use bevy_transform::TransformSystem;
 use draw::{OutsideFrustum, Visible};
@@ -52,7 +52,7 @@ use render_graph::{
     base::{self, BaseRenderGraphConfig, MainPass},
     RenderGraph,
 };
-use renderer::{AssetRenderResourceBindings, RenderResourceBindings};
+use renderer::{AssetRenderResourceBindings, RenderResourceBindings, RenderResourceContext};
 use shader::ShaderLoader;
 #[cfg(feature = "hdr")]
 use texture::HdrTextureLoader;
@@ -160,6 +160,10 @@ impl Plugin for RenderPlugin {
         .init_resource::<RenderResourceBindings>()
         .init_resource::<AssetRenderResourceBindings>()
         .init_resource::<ActiveCameras>()
+        .add_startup_system_to_stage(
+            StartupStage::PreStartup,
+            check_for_render_resource_context.system(),
+        )
         .add_system_to_stage(CoreStage::PreUpdate, draw::clear_draw_system.system())
         .add_system_to_stage(
             CoreStage::PostUpdate,
@@ -220,5 +224,13 @@ impl Plugin for RenderPlugin {
                 active_cameras.add(base::camera::CAMERA_2D);
             }
         }
+    }
+}
+
+fn check_for_render_resource_context(context: Option<Res<Box<dyn RenderResourceContext>>>) {
+    if context.is_none() {
+        panic!(
+            "bevy_render couldn't find a render backend. Perhaps try adding the bevy_wgpu plugin!"
+        )
     }
 }

commit 2ec7c00d5291474ff9e5423a22444ba5ab5f1445

don't panic when no RenderResourceContext can be found (#1971)
In bevy_webgl2, the `RenderResourceContext` is created after startup as it needs to first wait for an event from js side:
https://github.com/mrk-its/bevy_webgl2/blob/f31e5d49def7de6bfe58a47587e7ab3ca349da05/src/lib.rs#L117

remove `panic` introduced in #1965 and log as a `warn` instead

crates/bevy_render/src/lib.rs
@@ -17,6 +17,7 @@ use bevy_ecs::{
     system::{IntoExclusiveSystem, IntoSystem, Res},
 };
 use bevy_transform::TransformSystem;
+use bevy_utils::tracing::warn;
 use draw::{OutsideFrustum, Visible};
 
 pub use once_cell;
@@ -229,8 +230,8 @@ impl Plugin for RenderPlugin {
 
 fn check_for_render_resource_context(context: Option<Res<Box<dyn RenderResourceContext>>>) {
     if context.is_none() {
-        panic!(
+        warn!(
             "bevy_render couldn't find a render backend. Perhaps try adding the bevy_wgpu feature/plugin!"
-        )
+        );
     }
 }